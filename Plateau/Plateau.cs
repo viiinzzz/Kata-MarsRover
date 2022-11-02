69a895a46b8dd64666c26ebbf23e45303eada601
Vinz <vincentfontaine.jobs@gmail.com>
2022-11-02 19:03:33 +0100

    parse first line as plateau size. check_Plateau_size_when_initialized_with_string ok

--- Plateau/Plateau.cs
+++ Plateau/Plateau.cs
@@ -10,13 +10,18 @@ public record class Plateau(int Width, int Height)
 
     public Plateau(string config) : this(0, 0)
     {
-        //TODO: parse config
-        throw new NotImplementedException();
-        var size = "";
-            (Width, Height) = (
+        var lines = config.Split(new []{ '\r', '\n' },
+            StringSplitOptions.RemoveEmptyEntries)
+            .Select(line => line.Trim())
+            .Where(line => line.Length > 0)
+            .ToList();
+        var size = lines.First();
+        lines.RemoveAt(0);
+        (Width, Height) = (
             int.Parse(SizeRx.Match(size).Groups["Width"].Value),
             int.Parse(SizeRx.Match(size).Groups["Height"].Value)
         );
+        //TODO: parse fleet
     }
 
     public string Result => throw new NotImplementedException();

0bde38f7c0e3ad8d8566e3674c5219759a918846
Vinz <vincentfontaine.jobs@gmail.com>
2022-11-02 18:55:18 +0100

    augment Plateau to manage Rovers, constructor string param will not only be size but also the rover fleet config hence renaming to config. preparse config to lines

--- Plateau/Plateau.cs
+++ Plateau/Plateau.cs
@@ -5,13 +5,19 @@ namespace MarsRover;
 public record class Plateau(int Width, int Height)
 {
     private static readonly Regex SizeRx = new Regex(@"(?<Width>\d+) (?<Height>\d+)");
+    
+    private List<Rover> Rovers = new();
 
-    public Plateau(string size)
-        : this(0, 0)
-        => (Width, Height) = (
+    public Plateau(string config) : this(0, 0)
+    {
+        //TODO: parse config
+        throw new NotImplementedException();
+        var size = "";
+            (Width, Height) = (
             int.Parse(SizeRx.Match(size).Groups["Width"].Value),
             int.Parse(SizeRx.Match(size).Groups["Height"].Value)
         );
+    }
 
     public string Result => throw new NotImplementedException();
 }
